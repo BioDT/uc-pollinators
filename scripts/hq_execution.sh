1786e3e1300779b29862ef2045baca899dc6e26f
Tomas Martinovic <tomas.martinovic@vsb.cz>
2023-03-13 10:38:51 +0100

    updated hq execution script and data based on Karolina cluster test

--- scripts/hq_execution.sh
+++ scripts/hq_execution.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# We assume it is executed from the R project home directory.
-JAVA_HOME="/Users/martinovic/beehave/jdk-17.0.6.jdk/Contents/Home/"
-NETLOGO_HOME="/Users/martinovic/beehave/NetLogo 6.2.0"
-MODEL_PATH="${PWD}/data/Beehave_BeeMapp2015_Netlogo6version_PolygonAggregation.nlogo"
+ml R
 
-Rscript R/run_beehave.R "$JSON_INPUT" "$JAVA_HOME" "$NETLOGO_HOME" "$MODEL_PATH"
+JAVA_HOME="/scratch/project/open-27-31/jdk-17.0.5"
+NETLOGO_HOME="/scratch/project/open-27-31/NetLogo 6.2.0"
+MODEL_PATH="/scratch/project/open-27-31/uc-beehave-execution-scripts/data/Beehave_BeeMapp2015_Netlogo6version_PolygonAggregation.nlogo"
+
+Rscript R/run_beehave.R "${HQ_ENTRY}" "$JAVA_HOME" "$NETLOGO_HOME" "$MODEL_PATH"
+
