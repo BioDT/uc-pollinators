e716f3ab6bf580fab650d37ef1f2b5024f1e6dad
Tuomas Rossi <tuomas.rossi@csc.fi>
2023-04-26 12:31:48 +0300

    Split run script from submit script

--- scripts/submit_single.lumi.sh
+++ scripts/submit_single.lumi.sh
@@ -6,34 +6,12 @@
 #SBATCH --time=00:15:00
 
 # Use correct file paths here
-MODEL_PATH="data/Beehave_BeeMapp2015_Netlogo6version_PolygonAggregation.nlogo"
-SIF="beehave_0.3.1.sif"
-RSCRIPT="R/run_beehave.R"
+RUN="./scripts/run.lumi.sh"
 JSON_INPUT_PATH="data/single_execution.json"
 
 ####################################
 # No need to edit the lines below
 ####################################
 
-date
-echo $SLURM_JOB_ID
-
-# NetLogo pollutes home directory with .java and .netlogo directories, so
-# we use a temporary home directory in singularity
-TMP_HOME=`mktemp -d -p /tmp`
-
-# Create a base directory manually to suppress messages from NetLogo
-mkdir -p $TMP_HOME/.java/.userPrefs
-
-# Remove temporary home directory at exit
-# Note: One could do "rm -rf $TMP_HOME", but we don't do it as a safeguard in case
-# someone would modify TMP_HOME to be the real home and get everything erased there
-trap "rm -r $TMP_HOME/.java $TMP_HOME/.netlogo; rmdir $TMP_HOME" EXIT
-
-# Make model path env variable visible in Rscript
-export MODEL_PATH
-
-# Run nlrx
-singularity exec --home "$TMP_HOME" --bind "$PWD" "$SIF" Rscript "$RSCRIPT" "$(cat $JSON_INPUT_PATH)"
-
-date
+# Run
+"$RUN" "$(cat $JSON_INPUT_PATH)"
