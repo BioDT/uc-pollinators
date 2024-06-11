78aba75209b3c6813becabde3349c5a29c0a2758
fol0037 <ladislav.foltyn@vsb.cz>
2024-06-11 14:01:04 +0200

    update

--- scripts/cloud/run_docker_flow.sh
+++ scripts/cloud/run_docker_flow.sh
@@ -3,6 +3,18 @@
 export R_BOX_PATH="/"
 # export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64"
 
+export DATA_FOLDER="$1"
+export INPUT_DIR="/input/${DATA_FOLDER}"
+export OUTPUT_DIR="/output"
+export MAP="map.tif"
+export LOOKUP_TABLE="lookup_table.csv"
+export LOCATIONS="locations.csv"
+export PARAMETERS="parameters.csv"
+export SIMULATIONS="simulation.csv"
+export NETLOGO_JAR_PATH="/NetLogo 6.3.0/lib/app/netlogo-6.3.0.jar"
+export MODEL_PATH="${INPUT_DIR}/Beehave_BeeMapp2015_Netlogo6version_PolygonAggregation.nlogo" # This assumes model is put into the same folder as inputs
+export CPUS=1
+
 # Prepare JSON
 Rscript /R/step1_hq/step1_prepare_hq_jsons.R \
         -i "${INPUT_DIR}" \
@@ -18,7 +30,7 @@ Rscript /R/step1_hq/step1_prepare_hq_jsons.R \
 # https://docs.csc.fi/apps/hyperqueue/
 
 # HyperQueue server directory
-export HQ_SERVER_DIR="${PWD}/hq-server"
+export HQ_SERVER_DIR="/input/hq-server"
 mkdir -p "${HQ_SERVER_DIR}"
 
 # Remove the server directory at exit
